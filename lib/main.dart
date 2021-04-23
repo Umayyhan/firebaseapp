70c4948dc27bf68ad8ec31087d21da3b4ad3987b
yusuf cagdas yanar <yusuf.yanar@socradar.com>
2021-04-23 06:38:23 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:flutter_app/nav.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
--- lib/tip1_scrren.dart
+++ lib/tip1_scrren.dart
@@ -3,8 +3,37 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class Tip_1 extends StatelessWidget {
-  final databaseReference = FirebaseDatabase.instance.reference();
+  testFunc(){
+    var _list;
+//    Query _testRef = FirebaseDatabase.instance.reference().orderByChild("trip_distance").limitToFirst(50);
+//    DatabaseReference _testRef = FirebaseDatabase.instance.reference();
+    final databaseReference = FirebaseDatabase.instance.reference();
+    databaseReference
+        .orderByChild('trip_distance')
+        .once()
+        .then((snapshot) {
+      var firstPageItems = snapshot.value;
 
+      if (firstPageItems != null) {
+        List<dynamic> curretList = [];
+        firstPageItems.forEach((orderId, orderData) {
+
+          print ("date_slug " + orderData['date_slug'] + "\r\n\r\n\r\n");
+          curretList.add(orderData);
+        });
+
+        _list.addAll(curretList);
+      }
+    });
+//    _testRef.once().then((DataSnapshot snapshot){
+//      Map<dynamic, dynamic> values = snapshot.value;
+//      values.forEach((key,values) {
+//        print(values["name"]);
+//      });
+//    });
+//    databaseReference.child('0').once().then((value) => print(value));
+    print("hi");
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -13,7 +42,7 @@ class Tip_1 extends StatelessWidget {
         children: <Widget>[
           Text('data_2'),
           RaisedButton(onPressed: () {
-            databaseReference.child('0').once().then((value) => print(value));
+            testFunc();
           }),
         ],
       ),
